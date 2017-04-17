2ab193bdb6913a5277727aaa151ddf12b5571e19
Pedro Felipe Froes <pedrofelipefroes@Pedros-MacBook-Pro.local>
2017-04-17 19:53:41 -0300

    Adding temperature reading to project

--- basic-location/app/src/main/java/com/example/saulo/basiclocation/MainActivity.java
+++ basic-location/app/src/main/java/com/example/saulo/basiclocation/MainActivity.java
@@ -15,9 +15,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements LocationListener {
+public class MainActivity extends Activity implements LocationListener, SensorEventListener {
 
     private LocationManager locationManager;
+    private SensorManager sensorManager;
+    private Sensor temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,8 @@ public class MainActivity extends Activity implements LocationListener {
         }
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        temperature = sensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
 
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
                 2000, 1, this);
@@ -68,5 +72,32 @@ public class MainActivity extends Activity implements LocationListener {
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        float deviceTemperature = event.values[0];
+
+        String msg = "Device temperature: " + deviceTemperature;
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
