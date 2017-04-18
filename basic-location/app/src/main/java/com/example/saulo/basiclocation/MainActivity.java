15a6296bcb73e33eedb76c91109260e697672c6b
Saulo Antunes <sauloantuness@gmail.com>
2017-04-17 23:39:54 -0300

    Add sensor light

--- basic-location/app/src/main/java/com/example/saulo/basiclocation/MainActivity.java
+++ basic-location/app/src/main/java/com/example/saulo/basiclocation/MainActivity.java
@@ -5,6 +5,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,12 +18,15 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+import org.fusesource.mqtt.client.BlockingConnection;
+import org.fusesource.mqtt.client.MQTT;
+import org.fusesource.mqtt.client.QoS;
 
 public class MainActivity extends Activity implements LocationListener, SensorEventListener {
 
     private LocationManager locationManager;
     private SensorManager sensorManager;
-    private Sensor temperature;
+    private Sensor light;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,7 @@ public class MainActivity extends Activity implements LocationListener, SensorEv
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        temperature = sensorManager.getDefaultSensor(Sensor.TYPE_TEMPERATURE);
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
                 2000, 1, this);
@@ -46,10 +53,11 @@ public class MainActivity extends Activity implements LocationListener, SensorEv
     @Override
     public void onLocationChanged(Location location) {
 
-        String msg = "New Latitude: " + location.getLatitude()
-                + "New Longitude: " + location.getLongitude();
+        String msg = "Device Location: LAT(" + location.getLatitude()
+                + ") LONG( " + location.getLongitude() + ")";
 
         Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+        sendMessage("sd", msg);
     }
 
     @Override
@@ -83,15 +91,16 @@ public class MainActivity extends Activity implements LocationListener, SensorEv
     public final void onSensorChanged(SensorEvent event) {
         float deviceTemperature = event.values[0];
 
-        String msg = "Device temperature: " + deviceTemperature;
+        String msg = "Device light: " + deviceTemperature;
         Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+        sendMessage("sd", msg);
     }
 
     @Override
     protected void onResume() {
         // Register a listener for the sensor.
         super.onResume();
-        sensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -99,5 +108,17 @@ public class MainActivity extends Activity implements LocationListener, SensorEv
         // Be sure to unregister the sensor when the activity pauses.
         super.onPause();
         sensorManager.unregisterListener(this);
+    }
+
+    public void sendMessage(String topic, String message){
+        try {
+            MQTT mqtt = new MQTT();
+            mqtt.setHost("iot.eclipse.org", 1883);
+            BlockingConnection connection = mqtt.blockingConnection();
+            connection.connect();
+            connection.publish(topic, message.getBytes(), QoS.AT_LEAST_ONCE, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
